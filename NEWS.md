f0575ee152b2b49bb71d642918ffb646cf02822f
Trevor L Davis <trevor.l.davis@gmail.com>
2020-05-14 00:23:28 -0700

    chore: Add 'skip_on_ci()' before 'expect_doppelganger()'

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,4 @@
-tradgames 0.1.0
+tradgames 0.1.1
 ===============
 
 * Game start arrangement functions:
--- tests/testthat/test_game_starts.R
+++ tests/testthat/test_game_starts.R
@@ -4,6 +4,13 @@ library("vdiffr")
 
 context("test game diagrams")
 test_that("game diagrams work as expected", {
+    verify_output("../text_diagrams/american_checkers.txt", cat_piece(df_american_checkers()))
+    verify_output("../text_diagrams/crossings.txt", cat_piece(df_crossings()))
+    verify_output("../text_diagrams/focus.txt", cat_piece(df_focus()))
+    verify_output("../text_diagrams/four_field_kono.txt", cat_piece(df_four_field_kono()))
+    verify_output("../text_diagrams/lines_of_action.txt", cat_piece(df_lines_of_action()))
+    verify_output("../text_diagrams/turkish_draughts.txt", cat_piece(df_turkish_draughts()))
+    skip_on_ci()
     expect_doppelganger("american_checkers", function() {
         df <- df_american_checkers()
         pmap_piece(df, default.units = "in")
@@ -12,8 +19,4 @@ test_that("game diagrams work as expected", {
         df <- df_turkish_draughts()
         pmap_piece(df, default.units = "in")
     })
-    verify_output("../text_diagrams/crossings.txt", cat_piece(df_crossings()))
-    verify_output("../text_diagrams/focus.txt", cat_piece(df_focus()))
-    verify_output("../text_diagrams/four_field_kono.txt", cat_piece(df_four_field_kono()))
-    verify_output("../text_diagrams/lines_of_action.txt", cat_piece(df_lines_of_action()))
 })
